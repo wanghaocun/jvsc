67c3fe9d17ee12a73280f057cc7a65ee04aca579
wanghaocun <i@wanghaocun.com>
2022-04-25 18:59:44 +0800

    feat: add stream test project

---
+++ misc/stream-test/src/test/java/com/example/streamtest/StreamTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streamtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
