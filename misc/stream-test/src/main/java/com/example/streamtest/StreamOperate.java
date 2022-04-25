f14d9d70ac91a0b872c214e64576ae823469cb94
wanghaocun <i@wanghaocun.com>
2022-04-25 19:59:43 +0800

    feat: add stream get max value and summary statistics

---
+++ misc/stream-test/src/main/java/com/example/streamtest/StreamOperate.java
@@ -0,0 +1,40 @@
+package com.example.streamtest;
+
+import java.util.*;
+
+public class StreamOperate {
+
+    /**
+     * Stream.max(Integer::max)求取最大值
+     */
+    private static void max(List<Integer> numbers) {
+        // 取最大值  最小值同理
+        System.out.println(numbers.stream().max(Comparator.naturalOrder()).get());
+        Optional<Integer> reduce = numbers.stream().reduce(Integer::max);
+        System.out.println(reduce.orElse(0));
+        OptionalInt max = numbers.stream().mapToInt(Integer::intValue).max();
+        System.out.println(max.orElse(0));
+        System.out.println(numbers.stream().mapToInt(Integer::intValue).max().getAsInt());
+        System.out.println(numbers.stream().mapToInt(i -> i).max().getAsInt());
+
+    }
+
+    private static void summaryStatistics(List<Integer> numbers) {
+        // 概要统计
+        IntSummaryStatistics intSummaryStatistics = numbers.stream().mapToInt(Integer::intValue).summaryStatistics();
+        System.out.println("列表中最大的数 : " + intSummaryStatistics.getMax());
+        System.out.println("列表中最小的数 : " + intSummaryStatistics.getMin());
+        System.out.println("所有数之和 : " + intSummaryStatistics.getSum());
+        System.out.println("平均数 : " + intSummaryStatistics.getAverage());
+        System.out.println("平均数 : " + intSummaryStatistics.getCount());
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(-1, 2, 3, 4, 5);
+
+        max(numbers);
+        summaryStatistics(numbers);
+
+    }
+
+}
