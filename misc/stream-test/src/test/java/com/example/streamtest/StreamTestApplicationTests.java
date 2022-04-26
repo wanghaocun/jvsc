90203fa6b4516d278e0c28285a36499a908e4a1d
wanghaocun <i@wanghaocun.com>
2022-04-26 16:12:56 +0800

    feat: add redis zset exist value

--- misc/stream-test/src/test/java/com/example/streamtest/StreamTestApplicationTests.java
+++ misc/stream-test/src/test/java/com/example/streamtest/StreamTestApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.streamtest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.Objects;
 
 @SpringBootTest
 class StreamTestApplicationTests {
 
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
     @Test
-    void contextLoads() {
+    void zsetExistValue() {
+         redisTemplate.opsForZSet().add("plan:detection:task-queue", "234",123456);
+         redisTemplate.opsForZSet().add("plan:detection:task-queue", "2345",1234567);
+        Double score = redisTemplate.opsForZSet().score("plan:detection:task-queue", "2345");
+        Long rank = redisTemplate.opsForZSet().rank("plan:detection:task-queue", "2345");
+        System.out.println(Objects.isNull(score));
+        System.out.println(Objects.isNull(rank));
     }
+
 
 }
