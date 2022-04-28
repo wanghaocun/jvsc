a1ceefdf77a731de47e3ef34dc9ffd38c1a2849d
wanghaocun <i@wanghaocun.com>
2022-04-28 17:29:28 +0800

    feat: custom functional interface

--- misc/stream-test/src/main/java/com/example/streamtest/StreamOperate.java
+++ misc/stream-test/src/main/java/com/example/streamtest/StreamOperate.java
@@ -2,6 +2,7 @@ package com.example.streamtest;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamOperate {
 
@@ -30,6 +31,23 @@ public class StreamOperate {
         System.out.println("平均数 : " + intSummaryStatistics.getCount());
     }
 
+    /**
+     * 将筛选条件封装成接口
+     *
+     * @param apples
+     * @param filter
+     * @return
+     */
+    public static List<Apple> filterApplesByAppleFilter(List<Apple> apples, AppleFilter filter) {
+        List<Apple> filterApples = new ArrayList<>();
+        for (final Apple apple : apples) {
+            if (filter.accept(apple)) {
+                filterApples.add(apple);
+            }
+        }
+
+        return filterApples;
+    }
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(-1, 2, 3, 4, 5);
 
@@ -44,6 +62,19 @@ public class StreamOperate {
         System.out.println("======");
         List<String> strings = Arrays.stream(strs).map(s -> s.split("")).flatMap(Arrays::stream).distinct().toList();
         System.out.println(strings);
+
+        //Stream.generate(()->UUID.randomUUID().toString()).toList().forEach(System.out::println);
+
+        List<Apple> apples = Arrays.asList(
+                Apple.builder().id(1).color(Apple.Color.GREEN).origin("JS1").weight(1.5f).build(),
+                Apple.builder().id(2).color(Apple.Color.RED).origin("JS2").weight(2.5f).build(),
+                Apple.builder().id(3).color(Apple.Color.YELLOW).origin("JS3").weight(3.5f).build()
+        );
+
+        List<Apple> apples1 = filterApplesByAppleFilter(apples,
+                apple -> apple.getColor().equals(Apple.Color.GREEN)&&apple.getOrigin().equals("js"));
+        System.out.println(apples1);
+
     }
 
 }
