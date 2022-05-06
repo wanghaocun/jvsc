665984bf744fa2e359f56463a780008b4c730748
walnut <i@wanghaocun.com>
2022-05-06 22:30:07 +0800

    feat: add design patterns - adapter pattern examples

---
+++ design-patterns/src/main/java/com/example/designpatterns/StructuralPatterns/AdapterPattern/AdvancedMediaPlayer.java
@@ -0,0 +1,13 @@
+package com.example.designpatterns.StructuralPatterns.AdapterPattern;
+
+/**
+ * @author wanghaocun
+ * @since 2022-05-06
+ */
+public interface AdvancedMediaPlayer {
+
+    public void playVlc(String fileName);
+
+    public void playMp4(String fileName);
+
+}
---
+++ design-patterns/src/main/java/com/example/designpatterns/StructuralPatterns/AdapterPattern/MediaPlayer.java
@@ -0,0 +1,11 @@
+package com.example.designpatterns.StructuralPatterns.AdapterPattern;
+
+/**
+ * @author wanghaocun
+ * @since 2022-05-06
+ */
+public interface MediaPlayer {
+
+    public void play(String audioType, String fileName);
+   
+}
