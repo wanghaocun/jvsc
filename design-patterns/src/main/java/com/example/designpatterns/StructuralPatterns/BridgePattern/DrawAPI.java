552780b683110ae89838aea96fae771525f7371a
walnut <i@wanghaocun.com>
2022-05-07 11:25:03 +0800

    feat: add bridge pattern examples

---
+++ design-patterns/src/main/java/com/example/designpatterns/StructuralPatterns/BridgePattern/DrawAPI.java
@@ -0,0 +1,11 @@
+package com.example.designpatterns.StructuralPatterns.BridgePattern;
+
+/**
+ * @author wanghaocun
+ * @since 2022-05-07
+ */
+public interface DrawAPI {
+
+    public void drawCircle(int radius, int x, int y);
+
+}
