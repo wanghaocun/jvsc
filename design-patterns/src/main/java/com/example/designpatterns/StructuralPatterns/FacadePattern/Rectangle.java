470b4a7c619357f3eb1467874e3e1312637f7830
walnut <i@wanghaocun.com>
2022-05-09 19:55:41 +0800

    feat: add facade pattern examples

---
+++ design-patterns/src/main/java/com/example/designpatterns/StructuralPatterns/FacadePattern/Rectangle.java
@@ -0,0 +1,13 @@
+package com.example.designpatterns.StructuralPatterns.FacadePattern;
+
+/**
+ * @author wanghc
+ */
+public class Rectangle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Rectangle::draw()");
+    }
+
+}
