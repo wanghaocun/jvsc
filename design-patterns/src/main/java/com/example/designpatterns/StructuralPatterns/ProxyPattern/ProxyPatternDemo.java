54fce0fcd9b3000238822fa40a58a78f55011b3a
hermit-crab <i@haocun.wang>
2022-05-09 17:59:10 +0800

    feat: add proxy pattern examples

---
+++ design-patterns/src/main/java/com/example/designpatterns/StructuralPatterns/ProxyPattern/ProxyPatternDemo.java
@@ -0,0 +1,18 @@
+package com.example.designpatterns.StructuralPatterns.ProxyPattern;
+
+/**
+ * @author wanghc
+ */
+public class ProxyPatternDemo {
+
+    public static void main(String[] args) {
+        Image image = new ProxyImage("test_10mb.jpg");
+
+        // 图像将从磁盘加载
+        image.display();
+        System.out.println();
+        // 图像不需要从磁盘加载
+        image.display();
+    }
+
+}
