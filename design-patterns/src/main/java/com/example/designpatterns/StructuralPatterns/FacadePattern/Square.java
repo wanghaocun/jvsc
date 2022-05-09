f3f934a3d9d2791ece0c153d1d7cd377f75c2420
walnut <i@wanghaocun.com>
2022-05-09 19:56:01 +0800

    feat: add facade pattern examples

---
+++ design-patterns/src/main/java/com/example/designpatterns/StructuralPatterns/FacadePattern/Square.java
@@ -0,0 +1,13 @@
+package com.example.designpatterns.StructuralPatterns.FacadePattern;
+
+/**
+ * @author wanghc
+ */
+public class Square implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Square::draw()");
+    }
+
+}
