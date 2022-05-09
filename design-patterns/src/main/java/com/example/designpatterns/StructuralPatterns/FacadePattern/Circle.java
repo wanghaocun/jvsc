eac7da1befae1c0a7297b1cd4d1cefde3db88d02
walnut <i@wanghaocun.com>
2022-05-09 19:56:22 +0800

    feat: add facade pattern examples

---
+++ design-patterns/src/main/java/com/example/designpatterns/StructuralPatterns/FacadePattern/Circle.java
@@ -0,0 +1,13 @@
+package com.example.designpatterns.StructuralPatterns.FacadePattern;
+
+/**
+ * @author wanghc
+ */
+public class Circle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Circle::draw()");
+    }
+
+}
