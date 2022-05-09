db61aaeb8f93324eba5828e4bfea91f9d3ce55a3
hermit-crab <i@haocun.wang>
2022-05-09 17:14:05 +0800

    feat: add decorator pattern examples

---
+++ design-patterns/src/main/java/com/example/designpatterns/StructuralPatterns/DecoratorPattern/ShapeDecorator.java
@@ -0,0 +1,18 @@
+package com.example.designpatterns.StructuralPatterns.DecoratorPattern;
+
+/**
+ * @author wanghc
+ */
+public abstract class ShapeDecorator implements Shape {
+
+    protected Shape decoratedShape;
+
+    public ShapeDecorator(Shape decoratedShape) {
+        this.decoratedShape = decoratedShape;
+    }
+
+    public void draw() {
+        decoratedShape.draw();
+    }
+
+}
