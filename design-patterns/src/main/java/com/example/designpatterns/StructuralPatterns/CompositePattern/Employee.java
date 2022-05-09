67ed160bdf80fa18d99ef7d10c9f8b636d3970b2
walnut <i@wanghaocun.com>
2022-05-09 11:39:55 +0800

    feat: add composite pattern examples

---
+++ design-patterns/src/main/java/com/example/designpatterns/StructuralPatterns/CompositePattern/Employee.java
@@ -0,0 +1,43 @@
+package com.example.designpatterns.StructuralPatterns.CompositePattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wanghc
+ * @date 2022-5-9
+ */
+public class Employee {
+
+    private final String name;
+    private final String dept;
+    private final int salary;
+    private final List<Employee> subordinates;
+
+    // 构造函数
+    public Employee(String name, String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    public String toString() {
+        return ("Employee :[ Name : " + name
+                + ", dept : " + dept + ", salary :"
+                + salary + " ]");
+    }
+
+}
