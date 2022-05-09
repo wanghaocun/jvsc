5c2f82255d7e9d68c57884612e6cecaccd64fa2e
hermit-crab <i@haocun.wang>
2022-05-09 17:13:21 +0800

    feat: add decorator pattern examples

---
+++ design-patterns/src/main/java/com/example/designpatterns/StructuralPatterns/DecoratorPattern/Circle.java
@@ -0,0 +1,13 @@
+package com.example.designpatterns.StructuralPatterns.DecoratorPattern;
+
+/**
+ * @author wanghc
+ */
+public class Circle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Shape: Circle");
+    }
+
+}
