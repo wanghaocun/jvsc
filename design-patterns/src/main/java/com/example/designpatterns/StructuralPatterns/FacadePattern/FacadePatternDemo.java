f4ad55aba400eb7d4e02989f72f521cdcb9606d3
walnut <i@wanghaocun.com>
2022-05-09 19:57:40 +0800

    feat: add facade pattern examples

---
+++ design-patterns/src/main/java/com/example/designpatterns/StructuralPatterns/FacadePattern/FacadePatternDemo.java
@@ -0,0 +1,16 @@
+package com.example.designpatterns.StructuralPatterns.FacadePattern;
+
+/**
+ * @author wanghc
+ */
+public class FacadePatternDemo {
+
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+
+}
