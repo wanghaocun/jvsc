ba0cf11c0d50e262de24d1f8529b053c017d8261
hermit-crab <i@haocun.wang>
2022-05-09 17:55:57 +0800

    feat: add proxy pattern examples

---
+++ design-patterns/src/main/java/com/example/designpatterns/StructuralPatterns/ProxyPattern/RealImage.java
@@ -0,0 +1,24 @@
+package com.example.designpatterns.StructuralPatterns.ProxyPattern;
+
+/**
+ * @author wanghc
+ */
+public class RealImage implements Image {
+
+    private final String fileName;
+
+    public RealImage(String fileName) {
+        this.fileName = fileName;
+        loadFromDisk(fileName);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying " + fileName);
+    }
+
+    private void loadFromDisk(String fileName) {
+        System.out.println("Loading " + fileName);
+    }
+
+}
