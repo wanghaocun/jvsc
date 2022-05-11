20f561ed916cb410973cc6acd43d8fd2bd586c3c
walnut <i@wanghaocun.com>
2022-05-11 11:03:55 +0800

    feat: add chain of responsibility pattern examples

---
+++ design-patterns/src/main/java/com/example/designpatterns/BehavioralPatterns/ChainOfResponsibilityPattern/ConsoleLogger.java
@@ -0,0 +1,17 @@
+package com.example.designpatterns.BehavioralPatterns.ChainOfResponsibilityPattern;
+
+/**
+ * @author wanghc
+ */
+public class ConsoleLogger extends AbstractLogger {
+
+    public ConsoleLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Standard Console::Logger: " + message);
+    }
+
+}
