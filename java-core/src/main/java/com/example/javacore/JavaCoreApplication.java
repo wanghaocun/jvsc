bf947a85376f0a626a1419060d12e4a216a4d31a
walnut <i@wanghaocun.com>
2022-05-12 12:08:37 +0800

    feat: add java core module

---
+++ java-core/src/main/java/com/example/javacore/JavaCoreApplication.java
@@ -0,0 +1,17 @@
+package com.example.javacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wanghc
+ * @date 2022-05-12
+ **/
+@SpringBootApplication
+public class JavaCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCoreApplication.class, args);
+    }
+
+}
---
+++ java-core/src/test/java/com/example/javacore/JavaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
