09f7f5e3d973f6fa84ddc1beb475c887cae69039
walnut <i@wanghaocun.com>
2022-05-12 12:09:28 +0800

    feat: add java core - pass by value demo

---
+++ java-core/src/main/java/com/example/javacore/PassByValue.java
@@ -0,0 +1,40 @@
+package com.example.javacore;
+
+import lombok.Data;
+
+/**
+ * @author wanghc
+ * @date 2022-05-12
+ **/
+public class PassByValue {
+
+    @Data
+    static class Person {
+        private String name;
+        private int age;
+    }
+
+    public static void PersonCrossTest1(Person person) {
+        System.out.println("传入的person的name：" + person.getName());
+        person.setName("我是张小龙");
+        System.out.println("方法内重新赋值后的name：" + person.getName());
+    }
+
+    static void PersonCrossTest2(Person person) {
+        System.out.println("传入的person的name：" + person.getName());
+        // 加多此行代码
+        person = new Person();
+        person.setName("我是张小龙");
+        System.out.println("方法内重新赋值后的name：" + person.getName());
+    }
+
+    static void main(String[] args) {
+        Person p = new Person();
+        p.setName("我是马化腾");
+        p.setAge(45);
+        //PersonCrossTest1(p);
+        PersonCrossTest2(p);
+        System.out.println("方法执行后的name：" + p.getName());
+    }
+
+}
