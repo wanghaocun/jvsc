a0b0e4cb17e05a2889929247036309b04f2fced5
walnut <i@wanghaocun.com>
2022-05-15 16:47:35 +0800

    feat: add xxl job demo module

---
+++ xxl-job-demo/src/main/java/com/example/xxljobdemo/XxlJobDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.xxljobdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wanghaocun
+ */
+@SpringBootApplication
+public class XxlJobDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobDemoApplication.class, args);
+    }
+
+}
