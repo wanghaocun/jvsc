eedbade3adb712b781d6cc72446de1cd7c987999
walnut <i@wanghaocun.com>
2022-05-16 17:13:58 +0800

    feat: add interpreter pattern examples

---
+++ design-patterns/src/main/java/com/example/designpatterns/behavioral/interpreterpattern/OrExpression.java
@@ -0,0 +1,21 @@
+package com.example.designpatterns.behavioral.interpreterpattern;
+
+/**
+ * @author wanghc
+ */
+public class OrExpression implements Expression {
+
+    private final Expression expr1;
+    private final Expression expr2;
+
+    public OrExpression(Expression expr1, Expression expr2) {
+        this.expr1 = expr1;
+        this.expr2 = expr2;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        return expr1.interpret(context) || expr2.interpret(context);
+    }
+
+}
