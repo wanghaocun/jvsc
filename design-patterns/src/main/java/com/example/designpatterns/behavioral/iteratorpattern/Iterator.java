1c95b13090c241c9b053bb888465487cffc2a1ce
walnut <i@wanghaocun.com>
2022-05-18 18:55:29 +0800

    feat: add iterator pattern examples

---
+++ design-patterns/src/main/java/com/example/designpatterns/behavioral/iteratorpattern/Iterator.java
@@ -0,0 +1,12 @@
+package com.example.designpatterns.behavioral.iteratorpattern;
+
+/**
+ * @author wanghc
+ */
+public interface Iterator {
+
+    public boolean hasNext();
+
+    public Object next();
+
+}
