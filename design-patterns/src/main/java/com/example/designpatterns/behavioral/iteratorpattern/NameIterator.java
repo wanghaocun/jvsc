ac65416696bbe219741fdc9cb0aa2debf9111393
walnut <i@wanghaocun.com>
2022-05-23 22:47:13 +0800

    feat: add iterator pattern examples

--- design-patterns/src/main/java/com/example/designpatterns/behavioral/iteratorpattern/NameIterator.java
+++ design-patterns/src/main/java/com/example/designpatterns/behavioral/iteratorpattern/NameIterator.java
@@ -1,23 +1,25 @@
 package com.example.designpatterns.behavioral.iteratorpattern;
 
+import java.util.List;
+
 /**
  * @author wanghaocun
  */
 public class NameIterator implements Iterator {
 
-    String[] names = {"Robert", "John", "Julie", "Lora"};
+    List<String> names = List.of("Robert", "John", "Julie", "Lora");
 
     int index;
 
     @Override
     public boolean hasNext() {
-        return index < names.length;
+        return index < names.size();
     }
 
     @Override
     public Object next() {
         if (this.hasNext()) {
-            return names[index++];
+            return names.get(index++);
         }
 
         return null;
