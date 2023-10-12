c97b34e59be9f1f9793aa9453c9e6ee4dab44577
walnut <i@wanghaocun.com>
2023-10-12 16:05:02 +0800

    feat: utc/iso datetime conversion

--- java-core/src/main/java/com/example/javacore/DateTimeOperation.java
+++ java-core/src/main/java/com/example/javacore/DateTimeOperation.java
@@ -1,14 +1,13 @@
 package com.example.javacore;
 
+import javax.validation.constraints.NotNull;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * @author wanghc
@@ -82,7 +81,6 @@ public class DateTimeOperation {
         String dateString = sdf.format(new Date());
         System.out.println(dateString);
 
-
         // 测试转换是否正确
         SimpleDateFormat formatter1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date date = formatter1.parse("2018-08-01 21:22:22");
@@ -90,14 +88,17 @@ public class DateTimeOperation {
         LocalDateTime convertLocalDateTime = date.toInstant().atOffset(ZoneOffset.of("+8")).toLocalDateTime();
         //将java8 的 java.time.LocalDateTime 转换为 java.util.Date，默认时区为东8区
         Date convertDate = Date.from(convertLocalDateTime.toInstant(ZoneOffset.of("+8")));
+        System.out.println(convertDate);
         Long localDateTimeSecond = convertLocalDateTime.toEpochSecond(ZoneOffset.of("+8"));
         Long dateSecond = date.toInstant().atOffset(ZoneOffset.of("+8")).toEpochSecond();
         System.out.println(dateSecond.equals(localDateTimeSecond));
 
         Date ss = new Date();
+        //获得时间的另一种方式，测试效果一样
+        Date aw = Calendar.getInstance().getTime();
+        System.out.println(aw);
         System.out.println("一般日期输出：" + ss);
         System.out.println("时间戳：" + ss.getTime());
-        Date aw = Calendar.getInstance().getTime();//获得时间的另一种方式，测试效果一样
         SimpleDateFormat format0 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String time = format0.format(ss.getTime());//这个就是把时间戳经过处理得到期望格式的时间
         System.out.println("格式化结果0：" + time);
@@ -105,13 +106,84 @@ public class DateTimeOperation {
         time = format1.format(ss.getTime());
         System.out.println("格式化结果1：" + time);
 
-        // UTC时间转换为本地时间
-        String str = "2022-08-01T07:35:23.310145928Z";
+        // UTC时间转换为本地时间 "2022-08-01T07:35:23.310145928Z";
+        String utcDateString = generateUtcDateString();
+        String finalDateString = convertUtcDateToLocalDate(utcDateString);
+        System.out.printf("utc: %s -> %s%n", utcDateString, finalDateString);
+
+        // ISO时间转换为本地时间 Thu Oct 12 07:39:09 UTC 2023
+        String isoDateString = generateIsoDateString();
+        String finalDateString2 = convertIsoDateToLocalDate(isoDateString);
+        System.out.printf("iso: %s -> %s%n", isoDateString, finalDateString2);
+    }
+
+    /**
+     * 生成一个格式为 "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS'Z'" 的 UTC 日期字符串。
+     *
+     * @return 生成的 UTC 日期字符串
+     */
+    private static String generateUtcDateString() {
+        LocalDateTime dateTime = LocalDateTime.now(ZoneId.of("UTC"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS'Z'");
+
+        return dateTime.format(formatter);
+    }
+
+    /**
+     * 将UTC时间转换为本地时间
+     *
+     * @param utcDateString UTC时间字符串，格式为 "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS'Z'"
+     */
+    private static String convertUtcDateToLocalDate(String utcDateString) {
+        // 定义UTC时间格式
         DateTimeFormatter utcTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS'Z'");
+        // 定义本地时间格式
         DateTimeFormatter commonFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime localDateTime = LocalDateTime.parse(str, utcTimeFormatter);
+        // 将UTC时间字符串转换为LocalDateTime对象
+        LocalDateTime localDateTime = LocalDateTime.parse(utcDateString, utcTimeFormatter);
+        // 打印转换后的本地时间
         System.out.println(localDateTime);
-        System.out.println(commonFormatter.format(localDateTime.plus(Duration.ofHours(8))));
+        // 本地时区为东八区 所以加上8小时
+        String finalDateString = commonFormatter.format(localDateTime.plus(Duration.ofHours(8)));
+        // 打印转换后的本地时间
+        System.out.println(finalDateString);
+
+        return finalDateString;
+    }
+
+    /**
+     * 生成一个格式为 "EEE MMM dd HH:mm:ss zzz yyyy" 的 ISO 日期字符串。
+     *
+     * @return 生成的 ISO 日期字符串
+     */
+    private static String generateIsoDateString() {
+        LocalDateTime dateTime = LocalDateTime.now(ZoneId.of("UTC"));
+        // 周四 10月 12 07:40:56 UTC 2023
+        // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy", Locale.CHINA).withZone(ZoneId.of("UTC"));
+        // Thu Oct 12 07:39:09 UTC 2023
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US).withZone(ZoneId.of("UTC"));
+
+        return dateTime.format(formatter);
+    }
+
+    /**
+     * 将 ISODate 时间字符串转换为本地日期时间字符串
+     * 例如 查询 MongoDB 的时间字符串默认就是 ISODate 格式的
+     *
+     * @param isoDateString ISODate 时间字符串，格式为 "EEE MMM dd HH:mm:ss zzz yyyy"
+     * @return 转换后的本地日期时间字符串，格式为 "yyyy-MM-dd HH:mm:ss"
+     */
+    @NotNull
+    private static String convertIsoDateToLocalDate(String isoDateString) {
+        // 将 ISODate 时间字符串解析为 LocalDateTime 对象
+        // ZonedDateTime originDateTime = ZonedDateTime.parse(isoDateString,DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy", Locale.CHINA)).withZoneSameInstant(ZoneId.systemDefault());
+        ZonedDateTime originDateTime = ZonedDateTime.parse(isoDateString, DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US)).withZoneSameInstant(ZoneId.systemDefault());
+        // 格式化为目标时间格式的字符串
+        String finalDateString = originDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        // 打印转换后的本地时间
+        System.out.println(finalDateString);
+
+        return finalDateString;
     }
 
 }
